5a2f44e189ad8655c158d2d37007e4151531ef0d
ak4m410x01 <abdallah10kamal@gmail.com>
2023-10-23 01:41:34 +0300

    implement home template page

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("<center><h1>Home Page</h1></center>")
+    return render(request, "home.html")
