1929206a2af7686f76c06048ea8ca497ffa0034a
ak4m410x01 <abdallah10kamal@gmail.com>
2023-10-24 08:34:02 +0300

    customize Post Model in Admin Panel

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,15 @@ from .models import Post
 
 # Register your models here.
 
-admin.site.register(Post)
+# admin.site.register(Post)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "auther", "publish", "status")
+    list_filter = ("status", "created", "publish", "auther")
+    search_fields = ("title", "body")
+    prepopulated_fields = {"slug": ("title",)}
+    raw_id_fields = ("auther",)
+    date_hierarchy = "publish"
+    ordering = ("status", "publish")
